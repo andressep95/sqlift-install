d2b4d397e5d3a5d573d7c9b7b3ea081c5e8ef17e
Andres Sepulveda <136264322+andressep95@users.noreply.github.com>
2024-12-06 02:18:56 +0000

    Fix base path routing

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,10 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://andressep95.github.io/sqlift-install',
-  base: '/', 
-  output: 'static', 
+  site: 'https://andressep95.github.io',
+  base: '/sqlift-install',
+  output: 'static',
+  build: {
+    assets: 'assets'
+  }
 });
