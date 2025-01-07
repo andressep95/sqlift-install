0d1ada37986b07e2fc5a18d34c4df8043a95b6c3
Andres Sepulveda <136264322+andressep95@users.noreply.github.com>
2025-01-07 03:52:49 +0000

    update cmd 2

--- windows-install.cmd
+++ windows-install.cmd
@@ -1,12 +1,10 @@
 @echo off
-chcp 65001 > nul
+chcp 65001>nul
 setlocal enabledelayedexpansion
 
-:: Configuración
 set "VERSION=1.0.0"
 set "INSTALL_DIR=%USERPROFILE%\.sqlift"
 
-:: Detectar arquitectura del sistema
 set "ARCH=x64"
 echo Detectando arquitectura del sistema...
 
@@ -20,13 +18,11 @@ if "%PROCESSOR_ARCHITECTURE%"=="ARM64" (
 set "BASE_URL=https://github.com/andressep95/SQLift/releases/download/v%VERSION%"
 set "EXE_URL=%BASE_URL%/sqlift-windows-%ARCH%.exe"
 
-:: Crear directorio de instalación
 if not exist "%INSTALL_DIR%" (
     echo Creando directorio de instalacion...
     mkdir "%INSTALL_DIR%"
 )
 
-:: Descargar SQLift
 echo Descargando SQLift para Windows (%ARCH%)...
 curl -L -o "%INSTALL_DIR%\sqlift.exe" "%EXE_URL%"
 if %errorlevel% neq 0 (
@@ -34,7 +30,6 @@ if %errorlevel% neq 0 (
     exit /b 1
 )
 
-:: Agregar al PATH
 echo Agregando SQLift al PATH...
 setx PATH "%PATH%;%INSTALL_DIR%"
 
